8707bc02df326e0d40c6d723eaeccc634cd58cbb
Andrew Erickson <aerickson14@gmail.com>
2020-03-23 20:55:47 -0600

    complex closure return type

--- SwiftUIByExample/ListView.swift
+++ SwiftUIByExample/ListView.swift
@@ -6,8 +6,12 @@ struct ListView: View {
 
     var body: some View {
         List {
-            ForEach(1 ... numRows, id: \.self) { _ in
-                Text("Hello, World")
+            ForEach(1 ... numRows, id: \.self) { row in
+                if row % 2 == 0 {
+                    return Text("Hello, World \(row)")
+                } else {
+                    return /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
+                }
             }
         }
     }
