12da42867ba67afc1139a6ee2be93fd480b5f321
Andrew Erickson <aerickson14@gmail.com>
2020-03-23 20:49:43 -0600

    multiple instance previews

--- SwiftUIByExample/ListView.swift
+++ SwiftUIByExample/ListView.swift
@@ -2,9 +2,11 @@ import SwiftUI
 
 struct ListView: View {
 
+    var numRows: Int
+
     var body: some View {
         List {
-            ForEach(1 ... 5, id: \.self) { _ in
+            ForEach(1 ... numRows, id: \.self) { _ in
                 Text("Hello, World")
             }
         }
@@ -14,7 +16,11 @@ struct ListView: View {
 struct ListView_Previews: PreviewProvider {
 
     static var previews: some View {
-        ListView()
-            .previewLayout(.fixed(width: 400, height: 300))
+        Group {
+            ListView(numRows: 5)
+                .previewLayout(.fixed(width: 400, height: 300))
+            ListView(numRows: 10)
+                .previewLayout(.fixed(width: 400, height: 300))
+        }
     }
 }
