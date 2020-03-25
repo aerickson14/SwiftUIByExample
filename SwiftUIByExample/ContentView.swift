f0e24d524c5198357636eac405282d6761fe03df
Andrew Erickson <aerickson14@gmail.com>
2020-03-24 18:26:40 -0600

    ForEach

--- SwiftUIByExample/ContentView.swift
+++ SwiftUIByExample/ContentView.swift
@@ -2,10 +2,21 @@ import SwiftUI
 
 struct ContentView: View {
 
+    var rules: [String] = [
+        "Don't forget to wash your hands",
+        "Don't touch your face",
+        "Practice physical distancing",
+        "No international travel",
+        "Work from home",
+        "Self quarantine if you're experiencing symptoms"
+    ]
+
     var body: some View {
         VStack {
             Text("Hello world!")
-            Text("Don't forget to wash your hands")
+            ForEach(rules, id: \.self) { rule in
+                Text(rule)
+            }
         }
     }
 }
